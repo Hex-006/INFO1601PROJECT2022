ca0adb7330eaf1e8a8b48b39159a94a0ff704dee
Hex-006 <mjah0218@gmail.com>
2022-04-04 23:16:52 -0400

    app setup 0

--- neo-library/src/main.js
+++ neo-library/src/main.js
@@ -3,8 +3,9 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+// import { initializeApp } from "firebase/app";
+// import { getAnalytics } from "firebase/analytics";
+import firebase from "firebase/app";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -21,7 +22,8 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
+// const app = initializeApp(firebaseConfig);
 // const analytics = getAnalytics(app);
+firebase.initializeApp(firebaseConfig);
 
 createApp(App).use(store).use(router).mount("#app");
