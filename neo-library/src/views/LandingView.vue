c32109668359f531a9ea2858adc74f3195c87c8e
Hex-006 <mjah0218@gmail.com>
2022-04-05 22:28:56 -0400

    added sections to landingview

--- neo-library/src/views/LandingView.vue
+++ neo-library/src/views/LandingView.vue
@@ -1,6 +1,20 @@
 <template>
   <div class="landing">
-    <h1>Testing</h1>
+    <section id="health">
+      <img class="hlthimg" src="health.jpg" width="95%" height="750px" />
+    </section>
+    <section id="reccommended-reads">
+      <h1><i>Recommended Reads This Month</i></h1>
+      <!-- <a
+        href="javascript:document.querySelector('#mydialog').setAttribute('open', true)"
+        img
+        alt="book1"
+        src="book1.jpg"
+        width="300px"
+        height="450px"
+      >
+      </a> -->
+    </section>
   </div>
 </template>
 <script>
