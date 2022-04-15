d728a3c4bf6f554e085d6a5a00c40238fd48f80b
Hex-006 <mjah0218@gmail.com>
2022-04-14 22:41:50 -0400

    page proof of concept (no book api)

--- src/components/bookCard.vue
+++ src/components/bookCard.vue
@@ -1,14 +1,47 @@
 <template>
-  <div>
-    <h3>Hallo</h3>
+  <div class="card">
+    <!-- <h2>hello</h2> -->
+    <div class="inf">
+      <h4>{{ post.bookTitle }}</h4>
+      <router-link class="link" to="#"> <h6>view book</h6> </router-link>
+    </div>
   </div>
 </template>
 
 <script>
+// import fav from "../assets/projicons/basic-favorite-star-svgrepo-com.svg";
+// import arrow from "../assets/projicons/arrow-forward-ui-svgrepo-com.svg";
 export default {
   name: "bookCard",
   props: ["post"],
+  components: {
+    // fav,
+    // arrow,
+  },
 };
 </script>
 
-<style></style>
+<style lang="scss" scoped>
+.card {
+  position: relative;
+  cursor: pointer;
+  display: flex;
+  flex-direction: column;
+  min-height: 320px;
+  transition: 0.5s ease all;
+  background-color: rgb(163, 33, 33); //<<alter color of book card here
+  &:hover {
+    transform: rotateZ(1deg) scale(1.05);
+    box-shadow: 0 4px 6px -1px rgba(0, 0, 0, 0.1),
+      0 2px 4px -1px rgba(0, 0, 0, 0.6);
+  }
+}
+.info {
+  display: flex;
+  flex-direction: column;
+  height: 100%;
+  z-index: 3;
+  padding: 32px 16px;
+  color: #000;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -10,26 +10,3 @@ firebase.auth().onAuthStateChanged(() => {
     createApp(App).use(store).use(router).mount("#app");
   }
 });
-// // Import the functions you need from the SDKs you need
-// import { initializeApp } from "firebase/app";
-// // import { getAnalytics } from "firebase/analytics";
-// // import firebase from "firebase/app";
-// // // TODO: Add SDKs for Firebase products that you want to use
-// // // https://firebase.google.com/docs/web/setup#available-libraries
-
-// // // Your web app's Firebase configuration
-// // // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-// const firebaseConfig = {
-//   apiKey: "AIzaSyCKfoa432lUhXLpBwTyzcVF4YmchJyizpw",
-//   authDomain: "neo-library-of-alexandria.firebaseapp.com",
-//   projectId: "neo-library-of-alexandria",
-//   storageBucket: "neo-library-of-alexandria.appspot.com",
-//   messagingSenderId: "988392661656",
-//   appId: "1:988392661656:web:0214043d27c46586c126ed",
-//   measurementId: "G-FLZ4RDKR48",
-// };
-
-// // Initialize Firebase
-// // const app = initializeApp(firebaseConfig);
-// // const analytics = getAnalytics(app);
-// firebase.initializeApp(firebaseConfig);
